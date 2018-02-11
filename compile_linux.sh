383e09369a38566946d52e7d5c9d6759675762a7
Dennis Roof <dennis.roof.it@gmail.com>
2018-02-11 23:03:37 +0000

    Improved linux makefile and added raspberry pi 3 build

--- compile_linux.sh
+++ compile_linux.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-rm friendband_linux_64
+#rm friendband_linux_64
 cd src
 make -f Makefile.std clean
 make -f Makefile.std
-mv friendband_linux_64 ../
+mv friendband_linux_64 ../friendband_raspberry_pi3
 cd ..
 ./friendband_linux_64
-
